758c11515f4ee3223b3345fe53a2c2250192962c
Seifeddine Largat <largatseif@outlook.com>
2026-03-11 23:51:07 +0100

    feat: add AWS icon SVG, update TypeScript configuration, and create worker configuration
    - Added a new AWS icon SVG file to the icons directory.
    - Updated tsconfig.json to include additional TypeScript definition files and Astro components.
    - Created a new worker-configuration.d.ts file for type definitions.
    - Added wrangler.jsonc for Cloudflare Workers configuration with compatibility settings and asset binding.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "node:url"
+import path from 'path';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +12,14 @@ export default defineConfig({
   integrations: [],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@cv': fileURLToPath(new URL("./src/data/cv.json", import.meta.url)),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      }
+    }
+  },
+
+  adapter: cloudflare()
 });
